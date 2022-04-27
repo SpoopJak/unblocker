9c49b20f8d55db33ce1f6bd1fd2a091f23f2f1d7
SpoopJak <79949523+SpoopJak@users.noreply.github.com>
2022-04-27 10:12:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-
+window.open(
+              "chrome-extension://ghlpmldmjjhmdgmneoaibbegkjjbonbk/pages/blockPage.html?blockurl={"cat_name":"<iframe src=https%3A%2F%2Fcdpn.io%2Fdublinschools-xyz%2Ffullpage%2FdyJmNBR>"}", "_blank");
+}
